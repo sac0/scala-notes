0cb05bb5c648a5d0f5cb52f713726e9b6b2bb571
sachin govind <bssachingovind@gmail.com>
2020-12-18 03:18:47 +0530

    scala beginners and advanced added

---
+++ src/akkapersistence/primer/PersistAsyncDemo.scala
@@ -0,0 +1,64 @@
+package akkapersistence.primer
+
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.persistence.PersistentActor
+
+object PersistAsyncDemo extends App {
+
+
+  case class Command(contents: String)
+  case class Event(contents: String)
+
+  object CriticalStreamProcessor {
+    def props(eventAggregator: ActorRef): Props = Props(new CriticalStreamProcessor(eventAggregator))
+  }
+
+  class CriticalStreamProcessor(eventAggregator: ActorRef) extends PersistentActor with ActorLogging {
+
+    override def persistenceId: String = "critical-stream-processor"
+
+    override def receiveCommand: Receive = {
+
+          //send event to event aggregator which is a simple aggregator of data
+      case Command(contents) =>
+        eventAggregator ! s"Processing $contents"
+        // mutate
+        persistAsync(Event(contents)) /*                          TIME GAP                            */ { e =>
+          eventAggregator ! e
+          // mutate
+        }
+
+        // some actual computation
+        val processedContents = contents + "_processed"
+        persistAsync(Event(processedContents)) /*                          TIME GAP                            */ { e =>
+          eventAggregator ! e
+        }
+    }
+
+    override def receiveRecover: Receive = {
+      case message => log.info(s"Recovered: $message")
+    }
+  }
+
+  class EventAggregator extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(s"$message")
+    }
+  }
+
+
+  val system = ActorSystem("PersistAsyncDemo")
+  val eventAggregator = system.actorOf(Props[EventAggregator], "eventAggregator")
+  val streamProcessor = system.actorOf(CriticalStreamProcessor.props(eventAggregator), "streamProcessor")
+
+  streamProcessor ! Command("command1")
+  streamProcessor ! Command("command2")
+
+  /*
+    persistAsync vs persist
+    - perf: high-throughput environments
+    persist vs persisAsync
+    - ordering guarantees
+   */
+
+}
