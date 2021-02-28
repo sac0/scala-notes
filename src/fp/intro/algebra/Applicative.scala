21f4f7c44ca6a77668e27117422b11aef4c88936
sachin govind <bssachingovind@gmail.com>
2021-02-28 19:26:44 +0530

    Applicatives

---
+++ src/fp/intro/algebra/Applicative.scala
@@ -0,0 +1,226 @@
+package fp.intro.algebra
+
+import fp.intro.laziness.Stream
+
+object Applicative {
+
+  /**
+   * What you may not have noticed is that a large number of the
+   * useful combinators on Monad can be defined using only unit and map2.
+   * The traverse combinator is one example—it doesn’t call flatMap directly
+   * and is therefore agnostic to whether map2 is primitive or derived.
+   * Furthermore, for many data types,
+   * map2 can be implemented directly, without using flatMap.
+   * All this suggests a variation on Monad—the Monad interface has
+   * flatMap and unit as primitives, and derives map2,
+   * but we can obtain a different abstraction by letting unit
+   * and map2 be the primitives. We’ll see that this new abstraction,
+   * called an applicative functor, is less powerful than a monad,
+   * but we’ll also see that limitations come with benefits.
+   *
+   * def map2[A,B,C](ma: F[A], mb: F[B])(f: (A,B) => C): F[C] =
+   * flatMap(ma)(a => map(mb)(b => f(a,b)))
+   */
+
+  /**
+   * def join[A](f: F- F -A ): F[A]
+   * This peels a layer of F. Unit adds a layer and map2 just combines layers.
+   * There is not way to achieve join with just unit and map2 primitives
+   * applicative has no means of implementing flatmap as well since there is no flattening function
+   *
+   * We might say that with Applicative,
+   * the structure of our computation is fixed;
+   * with Monad, the results of previous computations
+   * may influence what computations to run next.
+   *
+   * If we want the result of one lookup to affect what lookup we do next, then we need flatMap or join.
+   *
+   *
+   * The differences
+   *  Applicative computations have fixed structure and simply sequence effects,
+   * whereas monadic computations may choose structure dynamically,
+   * based on the result of previous effects.
+   *
+   *  Applicative constructs context-free computations,
+   * while Monad allows for context sensitivity
+   *
+   *  Monad makes effects first class;
+   * they may be generated at “interpretation” time
+   * rather than chosen ahead of time by the program.
+   */
+
+  /**
+   * Advantages of applicative functors
+   *
+   * It is not as powerful as monad hence gives more flexibility on the implementations end
+   */
+
+  /**
+   * Stream is not an applicative functor
+   */
+  val streamApplicative: Applicative[Stream] = new Applicative[Stream] {
+
+    def unit[A](a: => A): Stream[A] =
+      Stream.constant(a) // The infinite, constant stream
+
+    override def map2[A, B, C](a: Stream[A], b: Stream[B])( // Combine elements pointwise
+                                                            f: (A, B) => C): Stream[C] =
+      a zip b map f.tupled
+  }
+
+  def validationApplicative[E]: Applicative[({type f[x] = Validation[E, x]})#f] =
+    new Applicative[({type f[x] = Validation[E, x]})#f] {
+      def unit[A](a: => A): Success[A] = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] =
+        (fa, fb) match {
+          case (Success(a), Success(b)) => Success(f(a, b))
+          case (Failure(h1, t1), Failure(h2, t2)) =>
+            Failure(h1, t1 ++ Vector(h2) ++ t2)
+          case (e@Failure(_, _), _) => e
+          case (_, e@Failure(_, _)) => e
+        }
+    }
+
+  type Const[A, B] = A
+
+  implicit def monoidApplicative[M](M: Monoid[M]) =
+    new Applicative[({type f[x] = Const[M, x]})#f] {
+      def unit[A](a: => A): M = M.zero
+
+      override def apply[A, B](m1: M)(m2: M): M = M.op(m1, m2)
+    }
+
+  /**
+   * Applicative rules
+   *
+   * Obey functor laws
+   * map(v)(id) == v
+   * map(map(v)(g))(f) = map (v)(f compose g)
+   *
+   *
+   * Left identity: map2(unit(()), fa)((_,a) => a) == fa
+   * Right identity: map2(fa, unit(()))((a,_) => a) == fa
+   * Associativity: product(product(fa, fb),fc) == map(product(fa, product(fb, fc)))(assoc)
+   * Naturality: map2(a,b)(productF(f,g)) == product(map(a)(f), map(b)(g))
+   *
+   */
+
+  /**
+   * Applicatives compose, monads don't.
+   *
+   * Monads do compose, but the result might not be a monad.
+   * In contrast, the composition of two applicatives is necessarily an applicative.
+   * I suspect the intention of the original statement was that
+   * "Applicativeness composes, while monadness doesn't." Rephrased,
+   * "Applicative is closed under composition, and Monad is not."
+   */
+
+
+}
+
+trait Applicative[F[_]] extends Functor[F] {
+  // `map2` is implemented by first currying `f` so we get a function
+  // of type `A => B => C`. This is a function that takes `A` and returns
+  // another function of type `B => C`. So if we map `f.curried` over an
+  // `F[A]`, we get `F[B => C]`. Passing that to `apply` along with the
+  // `F[B]` will give us the desired `F[C]`.
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
+
+  // We simply use `map2` to lift a function into `F` so we can apply it
+  // to both `fab` and `fa`. The function being lifted here is `_(_)`,
+  // which is the same as the lambda notation `(f, x) => f(x)`. That is,
+  // It's a function that takes two arguments:
+  //   1. A function `f`
+  //   2. An argument `x` to that function
+  // and it simply applies `f` to `x`.
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)(_ (_))
+
+  def unit[A](a: => A): F[A]
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] =
+    apply(unit(f))(fa)
+
+  def sequence[A](fas: List[F[A]]): F[List[A]] =
+    traverse(fas)(fa => fa)
+
+  def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+
+  def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
+    sequence(List.fill(n)(fa))
+
+  def factor[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((_, _))
+
+  def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = {
+    val self = this
+    new Applicative[({type f[x] = (F[x], G[x])})#f] {
+      def unit[A](a: => A) = (self.unit(a), G.unit(a))
+
+      override def apply[A, B](fs: (F[A => B], G[A => B]))(p: (F[A], G[A])) =
+        (self.apply(fs._1)(p._1), G.apply(fs._2)(p._2))
+    }
+  }
+
+  // Here we simply use `map2` to lift `apply` and `unit` themselves from one
+  // Applicative into the other.
+  // If `self` and `G` both satisfy the laws, then so does the composite.
+  // The full proof can be found at
+  // https://github.com/runarorama/sannanir/blob/master/Applicative.v
+  def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = {
+    val self = this
+    new Applicative[({type f[x] = F[G[x]]})#f] {
+      def unit[A](a: => A) = self.unit(G.unit(a))
+
+      override def map2[A, B, C](fga: F[G[A]], fgb: F[G[B]])(f: (A, B) => C) =
+        self.map2(fga, fgb)(G.map2(_, _)(f))
+    }
+  }
+
+  def sequenceMap[K, V](ofa: Map[K, F[V]]): F[Map[K, V]] =
+    (ofa foldLeft unit(Map.empty[K, V])) { case (acc, (k, fv)) =>
+      map2(acc, fv)((m, v) => m + (k -> v))
+    }
+
+}
+
+/**
+ * Traversable vs Iterable
+ * Think of it as the difference between blowing and sucking.
+ *
+ * When you have call a Traversables foreach, or its derived methods,
+ * it will blow its values into your function one at a time - so it has control over the iteration.
+ * With the Iterator returned by an Iterable though, you suck the values out of it,
+ * controlling when to move to the next one yourself.
+ *
+ *
+ * Traverse maintains structure - all folds do not
+ * A traversal is similar to a fold in that both take some data structure
+ * and apply a function to the data within in order to produce a result.
+ * The difference is that traverse preserves the original structure,
+ * whereas foldMap discards the structure and replaces it with the
+ * operations of a monoid. Look at the signature
+ * Tree Option A  => Option Tree A,
+ * for instance. We’re preserving the Tree structure, not merely col- lapsing the values using some monoid.
+ */
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E] = Vector())
+  extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
+
+object Monad {
+  def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] =
+    new Monad[({type f[x] = Either[E, x]})#f] {
+      def unit[A](a: => A): Either[E, A] = Right(a)
+
+      override def flatMap[A, B](eea: Either[E, A])(f: A => Either[E, B]): Either[E, B] = eea match {
+        case Right(a) => f(a)
+        case Left(b) => Left(b)
+      }
+    }
+}
--- src/fp/intro/algebra/Monad.scala
+++ src/fp/intro/algebra/Monad.scala
@@ -5,7 +5,7 @@ import fp.intro.testing._
 import fp.intro.parallelism._
 import fp.intro.state._
 import fp.intro.parallelism.Par._
-import language.higherKinds
+import fp.intro.laziness.Stream
 
 /**
  * def map2[A,B,C](fa: Parser[A], fb: Parser[B])(f: (A,B) => C): Parser[C] =
@@ -50,21 +50,55 @@ trait Monad[M[_]] extends Functor[M] {
   def map2[A, B, C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] = {
+    // this is throwing a similar code error since RNG is also a monad if we go to duplicated code
+    lma.foldRight(unit(List[A]()))((ma, b) => map2(ma, b)(_ :: _))
+  }
 
-  def traverse[A, B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A, B](la: List[A])(f: A => M[B]): M[List[B]] =
+    sequence(la map f)
 
-  def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
+  // For `List`, the `replicateM` function will generate a list of lists.
+  // It will contain all the lists of length `n` with elements selected from the
+  // input list.
+  // For `Option`, it will generate either `Some` or `None` based on whether the
+  // input is `Some` or `None`. The `Some` case will contain a list of length `n`
+  // that repeats the element in the input `Option`.
+  // The general meaning of `replicateM` is described very well by the
+  // implementation `sequence(List.fill(n)(ma))`. It repeats the `ma` monadic value
+  // `n` times and gathers the results in a single value, where the monad `M`
+  // determines how values are actually combined.
 
-  def compose[A, B, C](f: A => M[B], g: B => M[C]): A => M[C] = ???
+  // Recursive version:
+  def _replicateM[A](n: Int, ma: M[A]): M[List[A]] =
+    if (n <= 0) unit(List[A]()) else map2(ma, _replicateM(n - 1, ma))(_ :: _)
 
-  // Implement in terms of `compose`:
-  def _flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = ???
+  // Using `sequence` and the `List.fill` function of the standard library:
+  def replicateM[A](n: Int, ma: M[A]): M[List[A]] =
+    sequence(List.fill(n)(ma))
 
-  def join[A](mma: M[M[A]]): M[A] = ???
 
-  // Implement in terms of `join`:
-  def __flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def compose[A, B, C](f: A => M[B], g: B => M[C]): A => M[C] =
+    a => flatMap(f(a))(g)
+
+  def _flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] =
+    compose((_: Unit) => ma, f)(())
+
+  def join[A](mma: M[M[A]]): M[A] = flatMap(mma)(ma => ma)
+
+  /**
+   * Implement the function filterM. It’s a bit like filter, except that instead of a function
+   * from A => Boolean, we have an A => F[Boolean]. (Replacing various ordinary functions
+   * like this with the monadic equivalent often yields interesting results.)
+   * Implement this function, and then think about what it means for various data types.
+   */
+  def filterM[A](ms: List[A])(f: A => M[Boolean]): M[List[A]] =
+    ms.foldRight(unit(List[A]()))((x, y) =>
+      compose(f, (b: Boolean) => if (b) map2(unit(x), y)(_ :: _) else y)(x))
+
+  //  compose(compose(f, g), h) == compose(f, compose(g, h))
+
+
 }
 
 case class Reader[R, A](run: R => A)
@@ -97,7 +131,7 @@ object Monad {
   }
 
   val streamMonad: Monad[Stream] = new Monad[Stream] {
-    def unit[A](a: => A) = Stream(a)
+    def unit[A](a: => A): Stream[A] = Stream(a)
 
     override def flatMap[A, B](ma: Stream[A])(f: A => Stream[B]): Stream[B] = ma flatMap f
   }
@@ -108,25 +142,85 @@ object Monad {
     override def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma flatMap f
   }
 
-  def stateMonad[S]: Nothing = ???
+  // Since `State` is a binary type constructor, we need to partially apply it
+  // with the `S` type argument. Thus, it is not just one monad, but an entire
+  // family of monads, one for each type `S`. One solution is to create a class
+  // `StateMonads` that accepts the `S` type argument and then has a _type member_
+  // for the fully applied `State[S, A]` type inside:
+  class StateMonads[S] {
+    type StateS[A] = State[S, A]
 
-  val idMonad: Monad[Id] = ???
+    // We can then declare the monad for the `StateS` type constructor:
+    val monad: Monad[StateS] = new Monad[StateS] {
+      def unit[A](a: => A): State[S, A] = State(s => (a, s))
+      override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+        st flatMap f
+    }
+  }
 
-  def readerMonad[R]: Nothing = ???
+  // But we don't have to create a full class like `StateMonads`. We can create
+  // an anonymous class inline, inside parentheses, and project out its type member,
+  // `lambda`:
+  def stateMonad[S]: Monad[({
+    type lambda[x] = State[S, x]
+  })#lambda] = new Monad[({type lambda[x] = State[S, x]})#lambda] {
+    def unit[A](a: => A): State[S, A] = State(s => (a, s))
+    override def flatMap[A,B](st: State[S, A])(f: A => State[S, B]): State[S, B] =
+      st flatMap f
+  }
+
+  val idMonad: Monad[Id] = new Monad[Id] {
+    def unit[A](a: => A): Id[A] = Id(a)
+    override def flatMap[A,B](ida: Id[A])(f: A => Id[B]): Id[B] = ida flatMap f
+  }
+
+  def getState[S]: State[S,S] = State(s => (s,s))
+  def setState[S](s: S): State[S,Unit] = State(_ => ((),s))
+
+  val F: Monad[({
+    type lambda[x] = State[Int, x]
+  })#lambda] = stateMonad[Int]
+
+  def zipWithIndex[A](as: List[A]): List[(Int,A)] =
+    as.foldLeft(F.unit(List[(Int, A)]()))((acc,a) => for {
+      xs <- acc
+      n  <- getState
+      _  <- setState(n + 1)
+    } yield (n, a) :: xs).run(0)._1.reverse
+
+  // The action of Reader's `flatMap` is to pass the `r` argument along to both the
+  // outer Reader and also to the result of `f`, the inner Reader. Similar to how
+  // `State` passes along a state, except that in `Reader` the "state" is read-only.
+
+  // The meaning of `sequence` here is that if you have a list of functions, you can
+  // turn it into a function that takes one argument and passes it to all the functions
+  // in the list, returning a list of the results.
+
+  // The meaning of `join` is simply to pass the same value as both arguments to a
+  // binary function.
+
+  // The meaning of `replicateM` is to apply the same function a number of times to
+  // the same argument, returning a list of the results. Note that if this function
+  // is _pure_, (which it should be), this can be exploited by only applying the
+  // function once and replicating the result instead of calling the function many times.
+  // This means the Reader monad can override replicateM to provide a very efficient
+  // implementation.
+
+  def readerMonad[R]: Monad[({
+    type f[x] = Reader[R, x]
+  })#f] = new Monad[({type f[x] = Reader[R,x]})#f] {
+    def unit[A](a: => A): Reader[R, A] = Reader(_ => a)
+    override def flatMap[A,B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] =
+      Reader(r => f(st.run(r)).run(r))
+  }
 }
 
 case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B] = ???
+  def map[B](f: A => B): Id[B] = Id(f(value))
 
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
 
 object Reader {
-  def readerMonad[R]: Monad[({
-    type f[x] = Reader[R, x]
-  })#f] = new Monad[({type f[x] = Reader[R, x]})#f] {
-    def unit[A](a: => A): Reader[R, A] = ???
-
-    override def flatMap[A, B](st: Reader[R, A])(f: A => Reader[R, B]): Reader[R, B] = ???
-  }
+  def ask[R]: Reader[R, R] = Reader(r => r)
 }
